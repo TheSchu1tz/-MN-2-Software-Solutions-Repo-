bd694e3f7573643ded1258ba7bc5a0b8575372ac
Mickael Osegueda <mickaelosegueda031@gmail.com>
2025-11-24 00:32:53 -0800

    Finished implementation of heuristic search, running tests

--- A-Star_Algorithm_P3/app/search_test.py
+++ A-Star_Algorithm_P3/app/search_test.py
@@ -1,11 +1,12 @@
 import numpy as np
+import itertools
 import components.balance_ship as bs
 from queue import PriorityQueue
 from components.data_types.container import Container
 
 # Node class represents a state in the puzzle and represents the state as a list
 class Node:
-    def __init__(self, state =None, parent=None, depth = 0, cost=0):
+    def __init__(self, state =None, parent=None, depth = 0, cost=0, g_func = 0):
         if state is None:   # If no state is given, set state to an empty list
             state = []
         elif not isinstance(state, np.ndarray):  # If state is not a numpy array, raise an error
@@ -14,6 +15,16 @@ class Node:
         self.parent = parent
         self.depth = depth
         self.cost = cost
+        self.g_func = g_func
+        self.h_func = calculate_heuristic(self.state)
+        self.f_func = self.g_func + self.h_func
+
+    def __lt__(self, other):
+        return self.f_func < other.f_func
+
+    # Just for hashing purposes
+    def map_string(self):
+        return str(self.state.flatten())    
     
     # Add possible moves to expanded_nodes
     def expand(self):
@@ -22,16 +33,22 @@ class Node:
         # Search for all possible containers to move
         movable_containers = []
         for column in range(bs.GRID_COLS):
-            for row in range(bs.GRID_ROWS):
-                if self.state[row][column] != bs.UNUSED and self.state[row][column] != bs.NAN: # Found a movable container
+            for row in range(bs.GRID_ROWS -1, -1, -1):
+                container = self.state[row][column]
+                if  container.item != bs.UNUSED and container.item != bs.NAN: # Found a movable container
                     movable_containers.append((row, column))
+                    break
         
         #For all movable containers, add all potential moves to expanded nodes
         for container in movable_containers:
             for column in range(bs.GRID_COLS):
+
+                if column == container[1] or self.state[bs.GRID_ROWS - 1][column].item != bs.UNUSED: #Skip same column or full columns
+                    continue
+
                 cost, new_state = bs.MoveToColumn(self.state, self.state[container[0]][container[1]], column)
-                new_heur = calculate_heuristic(new_state)# Calculate heuristic for new state
-                expanded_nodes.append((new_heur, Node(new_state, self.state, self.depth + 1, cost)))
+                
+                expanded_nodes.append(Node(new_state, self, self.depth + 1, cost, self.g_func + cost))
         
         return expanded_nodes
 
@@ -39,40 +56,87 @@ class Node:
             
 # Using modified CheckBalance function to calculate the heuristic
 def calculate_heuristic(state:np.ndarray):
-    sumLeft = 0
-    sumRight = 0
-    for i in range(bs.GRID_ROWS):
-        for j in range(bs.GRID_COLS // 2):
-            leftItem:Container = state[i][j]
-            rightItem:Container = state[i][bs.GRID_COLS // 2 + j]
-            sumLeft += leftItem.weight
-            sumRight += rightItem.weight
 
-    difference = abs(sumLeft - sumRight)
+    if bs.CheckBalance(state): # Edge Case
+        return 0
     
-    return difference
+    #Get Weights of both sides
+    left_weight = 0
+    right_weight = 0
+    mid_col = bs.GRID_COLS // 2
+
+    for y in range(bs.GRID_ROWS):
+        for x in range(bs.GRID_COLS):
+            container:Container = state[y][x]
+            if container and container.item != bs.UNUSED and container.item != bs.NAN:
+                if x < mid_col:
+                    left_weight += container.weight
+                else:
+                    right_weight += container.weight
+    
+    #Figure out who is heavier
+    if left_weight > right_weight:
+        heavy_cols = range(0, mid_col)     
+        light_cols = range(mid_col, bs.GRID_COLS) 
+    else:
+        heavy_cols = range(mid_col, bs.GRID_COLS)
+        light_cols = range(0, mid_col)
+
+    min_h = float('inf') 
+    
+    # Search for all movable containers on the heavy side
+    from_columns = []
+    for column in heavy_cols:
+        for row in range(bs.GRID_ROWS -1, -1, -1):
+            container = state[row][column]
+            if  container.item != bs.UNUSED and container.item != bs.NAN: # Found a movable container
+                from_columns.append(column)
+                break
+    
+    # Search for all possible columns on light side
+    to_columns = []
+    for column in light_cols:
+        if state[bs.GRID_ROWS - 1][column].item == bs.UNUSED:
+            to_columns.append(column)
+    
+    #Get the heuristic value
+    for start in from_columns:
+        for end in to_columns:
+            dist = abs(start - end)
+            if dist < min_h:
+                min_h = dist
+    return min_h
+    
 
 # Run the A* search algorithm on a given grid
 def run_search(starting_grid: np.ndarray):
 
+    tie_breaker = itertools.count() # This is just to break ties in the queue when two heuristic values are the same
     q = PriorityQueue()
     visited = set()
+
     init_state = Node(starting_grid) # Convert the grid to a Node type
-    init_heur = calculate_heuristic(init_state.state)
-    q.put((init_heur, init_state)) # Add initial state to the queue with heuristic function
+    
+    print(f"DEBUG: STARTING WITH INITIAL H VALUE OF {init_state.h_func}")
+    q.put((init_state.f_func, next(tie_breaker), init_state)) # Add initial state to the queue with heuristic function
 
     while(q): # Start searching by popping elements off the queue
-        _, curr_node = q.get()
-        visited.add(curr_node)
+        _, _, curr_node = q.get()
 
         if bs.CheckBalance(curr_node.state):
+            print(f"DEBUG: SHIP BALANCED. g = {curr_node.g_func}, h = {curr_node.h_func}")
             return curr_node.state
+        
+        state_id = curr_node.map_string()
+        if state_id in visited:
+            continue
+        visited.add(state_id)
         
         expanded_nodes = curr_node.expand()
         
-        for heur, node in expanded_nodes:
-            if node not in visited:
-                q.put((heur, node))
+        for node in expanded_nodes:
+            if node.map_string() not in visited:
+                q.put((node.f_func, next(tie_breaker), node))
 
     # This is an error condition
     return None
