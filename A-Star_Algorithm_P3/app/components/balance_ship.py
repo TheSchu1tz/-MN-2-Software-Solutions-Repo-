646a334ce849e5c7df93a6bae5300c4acc5a7299
TheSchu1tz <noahjschultz@gmail.com>
2025-11-23 20:12:22 -0800

    Finalized CheckBalance and fixed some import paths

--- A-Star_Algorithm_P3/app/components/balance_ship.py
+++ A-Star_Algorithm_P3/app/components/balance_ship.py
@@ -1,6 +1,6 @@
 import numpy
-from components.data_types.coordinate import Coordinate
-from components.data_types.container import Container
+from data_types.coordinate import Coordinate
+from data_types.container import Container
 
 GRID_ROWS = 8
 GRID_COLS = 12
@@ -47,12 +47,12 @@ def CheckBalance(grid:numpy.ndarray):
     # Get Weights
     left_weights = []
     right_weights = []
-    mid_col = GRID_COLS / 2
+    mid_col = GRID_COLS // 2
 
-    for x in range(GRID_ROWS):
-        for y in range(GRID_COLS):
-            container:Container = grid[x][y]
-            if container.item != UNUSED and container.item != NAN:
+    for y in range(GRID_ROWS):
+        for x in range(GRID_COLS):
+            container:Container = grid[y][x]
+            if container and container.item != UNUSED and container.item != NAN:
                 if x < mid_col:
                     left_weights.append(container.weight)
                 else:
@@ -99,9 +99,9 @@ def MinDiffBalanceHelper(sumLeft: int, sumRight: int, all_weights: int):
     possible_sums = {0}
 
     for weight in all_weights:
-        possible_sums |= {sum + weight for sum in possible_sums}
+        possible_sums |= {s + weight for s in possible_sums}
 
-    best_half_sum = max(sum for sum in possible_sums if sum <= target)
+    best_half_sum = max(s for s in possible_sums if s <= target)
     possible_min_diff = total_weight - best_half_sum - best_half_sum
     return difference == possible_min_diff
 
@@ -136,3 +136,82 @@ def ParseFile(lines):
         container = Container(coord, weight, item)
         manifest.append(container)
     return manifest
+
+# TEST MAIN
+# def main():
+#     print("--Test 1: 0 Containers Balanced (True, Special Case 1)--")
+#     fake_manifest_1 = [
+#         "[01,01], {00000}, UNUSED",
+#         "[01,12], {00000}, UNUSED"
+#     ]
+#     manifest_1 = ParseFile(fake_manifest_1)
+#     grid_1 = CreateGrid(manifest_1)
+#     print(f"Result 1: {CheckBalance(grid_1)}")
+
+#     print("--Test 2: 1 Container Balanced (True, Special Case 2)--")
+#     fake_manifest_2 = [
+#         "[01,01], {00100}, Place1",
+#         "[01,12], {00000}, UNUSED"
+#     ]
+#     manifest_2 = ParseFile(fake_manifest_2)
+#     grid_2 = CreateGrid(manifest_2)
+#     print(f"Result 2: {CheckBalance(grid_2)}")
+
+    
+#     print("--Test 3: 2 Container Balanced (True, Special Case 3)--")
+#     fake_manifest_3 = [
+#         "[01,01], {00100}, Place1",
+#         "[01,12], {01000}, Place2"
+#     ]
+#     manifest_3 = ParseFile(fake_manifest_3)
+#     grid_3 = CreateGrid(manifest_3)
+#     print(f"Result 3: {CheckBalance(grid_3)}")
+
+#     print("--Test 4: 2 Container Unbalanced (False, Both Same Side)--")
+#     fake_manifest_4 = [
+#         "[01,01], {00100}, Place1",
+#         "[01,02], {01000}, Place2"
+#     ]
+#     manifest_4 = ParseFile(fake_manifest_4)
+#     grid_4 = CreateGrid(manifest_4)
+#     print(f"Result 4: {CheckBalance(grid_4)}")
+
+#     print("--Test 5: 5 Container Balanced (True, Within 10%)--")
+#     fake_manifest_5 = [
+#         "[01,01], {00100}, Place1",
+#         "[02,01], {00100}, Place2",
+#         "[01,02], {00100}, Place3",
+#         "[01,12], {00150}, Place4",
+#         "[01,11], {00130}, Place5"
+#     ]
+#     manifest_5 = ParseFile(fake_manifest_5)
+#     grid_5 = CreateGrid(manifest_5)
+#     print(f"Result 5: {CheckBalance(grid_5)}")
+
+#     print("--Test 6: 5 Container Balanced (True, Difference Is Minimal (300-200))--")
+#     fake_manifest_6 = [
+#         "[01,01], {00100}, Place1",
+#         "[02,01], {00100}, Place2",
+#         "[01,02], {00100}, Place3",
+#         "[01,12], {00100}, Place4",
+#         "[01,11], {00100}, Place5"
+#     ]
+#     manifest_6 = ParseFile(fake_manifest_6)
+#     grid_6 = CreateGrid(manifest_6)
+#     print(f"Result 6: {CheckBalance(grid_6)}")
+
+#     print("--Test 7: 5 Container Unbalanced (False, Not 10% Or Minimal (400-100))--")
+#     fake_manifest_7 = [
+#         "[01,01], {00100}, Place1",
+#         "[02,01], {00100}, Place2",
+#         "[01,02], {00100}, Place3",
+#         "[02,02], {00100}, Place4",
+#         "[01,12], {00100}, Place5"
+#     ]
+#     manifest_7 = ParseFile(fake_manifest_7)
+#     grid_7 = CreateGrid(manifest_7)
+#     print(f"Result 7: {CheckBalance(grid_7)}")
+
+    
+# if __name__ == "__main__":
+#     main()
